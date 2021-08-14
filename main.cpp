175ee2d18aca1310bacf4f740d47f815c56bb981
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-08-14 21:30:43 +0300

    Support tests

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 void generate(Goal* g);
 void print_ast(Goal* g);
+vector<string> splitString(string s, char delimiter);
 
 int main(int argc, char** argv)
 {
@@ -23,7 +24,13 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    input.open(argv[1]);
+    string path = argv[1];
+    string filename = splitString(path, '/').back();
+    filename.resize(filename.length() - 2);
+    string new_file_path = path;
+    new_file_path.resize(path.length() - 2);
+
+    input.open(path);
 
     Lexer lex(input);
     auto tokens = lex.lex();
@@ -33,7 +40,18 @@ int main(int argc, char** argv)
 
     pid_t pid = fork();
     if (pid == 0) {
-        int i = execl("/usr/bin/gcc", "gcc", "-m32", "./assembly.s", "-o", "out", NULL);
+        int i = execl("/usr/bin/gcc", "gcc", "-m32", "./assembly.s", "-o", filename.c_str(), NULL);
+        if (i) {
+            std::cout << errno << std::endl;
+        }
+    } else if (pid > 0) {
+        int status;
+        wait(&status);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        int i = execl("/usr/bin/cp", "cp", string("./" + filename).c_str(), new_file_path.c_str(), NULL);
         if (i) {
             std::cout << errno << std::endl;
         }
@@ -60,3 +78,24 @@ void print_ast(Goal* g)
 {
 
 }
+
+vector<string> splitString(string s, char delimiter)
+{
+    vector<string> strings {};
+    string currentString = "";
+    for (auto x : s) {
+        if (x == delimiter) {
+            strings.push_back(currentString);
+            currentString = "";
+        } else {
+            currentString += x;
+        }
+    }
+    if (currentString != "") {
+        strings.push_back(currentString);
+    }
+
+    return strings;
+}
+
+
